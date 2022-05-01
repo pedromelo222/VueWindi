9e1d31b79555ee126c86329bede93288184b0569
Pedro Melo <pedro.meloo@hotmail.com>
2022-05-01 11:46:00 -0300

    docs: update basic button

--- docs/components/button/demo/basic.vue
+++ docs/components/button/demo/basic.vue
@@ -1,6 +1,7 @@
 
 <script setup lang="ts">
 import { WButton } from "../../../../src"
+
 </script>
 
 <template> 
@@ -12,6 +13,9 @@ import { WButton } from "../../../../src"
         <WButton pills>Primary pills</WButton>
         <WButton variant="outline">Outline</WButton>
         <WButton variant="link">Link</WButton>
+        <WButton circle> 
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"> </path></svg> 
+        </WButton>
      <!-- #endregion snippet -->
 
     </div>
