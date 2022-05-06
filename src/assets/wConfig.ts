31a901c127b2a05d7ff11f22da039e5b87cf840a
Pedro Melo <pedro.meloo@hotmail.com>
2022-05-06 17:02:42 -0300

    feat: utility unset and inherit for border radius

--- src/assets/wConfig.ts
+++ src/assets/wConfig.ts
@@ -1,5 +1,6 @@
 import colors from "windicss/colors";
 import { varToRgba } from "../helper/index";
+import plugin from 'windicss/plugin'
 
 export default {
   preflight: {
@@ -43,4 +44,37 @@ export default {
       },    
     },
   },
+  
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.border-tl-inherit': {
+          'border-top-left-radius': 'inherit',
+        },
+        '.border-tr-inherit': {
+          'border-top-right-radius': 'inherit',
+        },
+        '.border-bl-inherit': {
+          'border-bottom-left-radius': 'inherit',
+        },
+        '.border-br-inherit': {
+          'border-bottom-right-radius': 'inherit',
+        },
+        '.border-tl-unset': {
+          'border-top-left-radius': 'unset',
+        },
+        '.border-tr-unset': {
+          'border-top-right-radius': 'unset',
+        },
+        '.border-bl-unset': {
+          'border-bottom-left-radius': 'unset',
+        },
+        '.border-br-unset': {
+          'border-bottom-right-radius': 'unset',
+        },
+
+
+      })
+    }),
+  ]
 };
