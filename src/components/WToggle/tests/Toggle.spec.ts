bb6de37f3783778c844b5671da4d934f27ebee38
Pedro Melo <pedro.meloo@hotmail.com>
2022-05-12 15:28:35 -0300

    fix: toggle disabled checked

---
+++ src/components/WToggle/tests/Toggle.spec.ts
@@ -0,0 +1,9 @@
+import { mount } from "@vue/test-utils";
+import WToggle from "../WToggle.vue";
+
+describe("WToggle", () => {
+  it("should work with import on demand", () => {
+    mount(WToggle);
+  });
+
+});
