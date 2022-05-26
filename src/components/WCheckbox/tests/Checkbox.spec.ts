c2b5c85aead136a1dff1f4e5075798a02d3c33d1
Pedro Melo <pedro.meloo@hotmail.com>
2022-05-26 17:47:35 -0300

    refactor(docs): remove docs from repository

---
+++ src/components/WCheckbox/tests/Checkbox.spec.ts
@@ -0,0 +1,9 @@
+import { mount } from "@vue/test-utils";
+import WCheckbox from "../WCheckbox.vue";
+
+describe("WCheckbox", () => {
+  it("should work with import on demand", () => {
+    mount(WCheckbox);
+  });
+
+});
