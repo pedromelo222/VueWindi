0ddf584c63f8871a095c5d2124b20aa53c0c4a5f
Pedro Melo <pedro.meloo@hotmail.com>
2022-06-07 09:47:03 -0300

    chore: cleanup unused theme vars

--- windi.config.ts
+++ windi.config.ts
@@ -6,16 +6,11 @@ export default defineConfig({
   plugins: [vueWindi],
   vueWindi: {
     themes: [
-      {
-        'primary-100': '#2563eb',
-        'primary-200': '#1d4ed8',
-        'primary-300': '#1e40af',
-      },
       'blue',
       'teal',
       'dracula',
-      'violet',
-      'orange',
+      'nord',
+      'neutral',
     ],
   },
 })
