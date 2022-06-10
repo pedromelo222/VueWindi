88e9c7bd12f99c52cbdae33497b3e19e4f086e30
Pedro Melo <pedro.meloo@hotmail.com>
2022-06-10 09:47:17 -0300

    test: add toggle tests

--- src/components/WToggle/tests/Toggle.spec.ts
+++ src/components/WToggle/tests/Toggle.spec.ts
@@ -5,4 +5,48 @@ describe('WToggle', () => {
   it('should work with import on demand', () => {
     mount(WToggle)
   })
+
+  it('clickable', async () => {
+    const onClick = vi.fn()
+    const wrapper = mount(WToggle, { attrs: { onClick } })
+    await wrapper.find('input').trigger('click')
+    expect(onClick).toHaveBeenCalled()
+    expect(wrapper.find('input').element.checked).toBeTruthy()
+    wrapper.unmount()
+  })
+
+  it('disabled', async () => {
+    const onClick = vi.fn()
+    const wrapper = mount(WToggle, {
+      attrs: { onClick },
+      props: { disabled: true },
+    })
+    await wrapper.find('input').trigger('click')
+    expect(onClick).not.toHaveBeenCalled()
+    wrapper.unmount()
+  })
+
+  it('should work with `color` prop', async () => {
+    (['primary', 'secondary', 'success', 'warning', 'danger'] as const).forEach(
+      (color) => {
+        const wrapper = mount(WToggle, { props: { color } })
+        expect(wrapper.find('input').classes()).toContain(
+          `w-toggle-${color}`,
+        )
+        wrapper.unmount()
+      },
+    )
+  })
+
+  it('should work with `size` prop', async () => {
+    (['xs', 'sm', 'md', 'lg'] as const).forEach(
+      (size) => {
+        const wrapper = mount(WToggle, { props: { size } })
+        expect(wrapper.find('input').classes()).toContain(
+          `w-toggle-${size}`,
+        )
+        wrapper.unmount()
+      },
+    )
+  })
 })
